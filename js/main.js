961dff80cc98ea608369898639c98feb5c051200
micky rendon <elderendon@hotmail.es>
2022-10-24 18:17:11 -0300

    random numbers for and more

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// global vars**
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const symbols = ['~','!','@','#','$','%','^','&','*','|', '/','"',':','<','>','?']
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+const arrs = [numbers, symbols, alphabet]
+const pswdLength = 10
+const strongpswd = []
+const randomNumbers = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1))
+}
+
+for (let i; i , pswdLength; i++){
+    const limits = arrs[randomNumbers(0, arrs.length -1)]
+    const rndmChar = limits[randomNumbers(0, limits.length -1)]
+    // strongpswd.push(rndmChar)
+    // return console.log(strongpswd)
+}
