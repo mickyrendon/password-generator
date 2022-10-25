0872bc34348e043e29231c13bdec26c02f757532
micky rendon <elderendon@hotmail.es>
2022-10-25 20:47:49 -0300

    strong password

--- js/main.js
+++ js/main.js
@@ -6,13 +6,19 @@ const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 const arrs = [numbers, symbols, alphabet]
 const pswdLength = 10
 const strongpswd = []
+
+const paswdF = document.querySelector('.app')
+
 const randomNumbers = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1))
 }
 
-for (let i; i , pswdLength; i++){
+for (i = 0; i < pswdLength; i++){
     const limits = arrs[randomNumbers(0, arrs.length -1)]
     const rndmChar = limits[randomNumbers(0, limits.length -1)]
-    // strongpswd.push(rndmChar)
-    // return console.log(strongpswd)
+
+    strongpswd.push(rndmChar)   
 }
+const finalPswd = strongpswd.join('')
+
+paswdF.textContent = finalPswd  
